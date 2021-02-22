e2aebb77e97a4ab451b63537ce2fd06f750b9f38
Muyiwa Olalekan <muyiwa.olalekan@thebulb.africa>
2021-02-22 14:01:18 +0100

    Add project files.

---
+++ Utilities/Savings.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApplication4.Utilities
+{
+    public class Savings
+    {
+        public decimal InterestCalculator(decimal p, decimal r, decimal t)
+        {
+            return (p * r * t / 100) + p;
+        }
+    }
+}
